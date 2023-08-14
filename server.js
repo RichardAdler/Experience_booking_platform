bf1122aceaf58488aff956433d892155095603bb
Richard Adler <RichardAdler@brianjames.local>
2023-08-14 16:03:42 +0100

    Database created, sqlite3 database connection established.

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 
+app.use(express.json()); //
+app.use(express.urlencoded({ extended: false})); 
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`The application is running on the following port: ${port}.`);
 })
